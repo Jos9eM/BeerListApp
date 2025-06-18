c73a257682ca7ec1db9556dc87851474742dc1c6
Josue Monzon <josue.monzon.f@gmail.com>
2025-06-18 17:26:23 -0600

    Init Project

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BeerListApp"
+
+include(":app")
+include(":core")
+include(":home")
+include(":home:home_data")
+include(":home:home_domain")
+include(":home:home_presentation")
